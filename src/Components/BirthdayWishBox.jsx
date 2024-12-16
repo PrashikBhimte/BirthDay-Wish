cb72a11ef32e2f9d635cab19716bc89e98f70b7b
PrashikBhimte <prashikbhimte29@gmail.com>
2024-12-16 22:53:41 +0530

    bug solved.

--- src/Components/BirthdayWishBox.jsx
+++ src/Components/BirthdayWishBox.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import "./BirthdayWishBox.css";
+import head from "../Images/head.png";
 
 export default function BirthdayWishBox() {
 
   return (
     <div id='birthdayWishBox'>
-      <div id='birthdayWishBoxImg'></div>
+      <img id='birthdayWishBoxImg' src={head} alt='happy birthday' />
+      {/* <div ></div> */}
       <p>Happy Birthday Gargi</p>
     </div>
   )
